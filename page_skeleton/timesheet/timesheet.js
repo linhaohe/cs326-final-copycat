0130a03209f6bf1d2a38f4a614812520b94263e5
minhnghia2208 <nghiapham@umass.edu>
2022-05-02 13:13:14 -0400

    Timesheet: Refactor html script to js

--- page_skeleton/timesheet/timesheet.js
+++ page_skeleton/timesheet/timesheet.js
@@ -213,3 +213,107 @@ document.getElementById('select').addEventListener('click', async () => {
         renderRow(tbl, data[i]);
     }
 });
+
+// MODAL SECTION
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("filter");
+var cancel = document.getElementById("cancel");
+var apply = document.getElementById("apply");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+cancel.onclick = function() {
+    modal.style.display = "none";
+}
+apply.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+const cmonth = document.getElementById("cmonth");
+cmonth.onclick = function() {
+    const now = new Date();
+    const fmonth = document.getElementById("fmonth");
+    const tmonth = document.getElementById("tmonth");
+
+    fmonth.value = now.getMonth();
+    tmonth.value = now.getMonth() + 1;
+}
+
+const today = document.getElementById("today");
+today.onclick = function() {
+    const now = new Date();
+    const fday = document.getElementById("fday");
+    const tday = document.getElementById("tday");
+
+    fday.value = now.getDate() - 1;
+    tday.value = now.getDate();
+}
+
+const lhour = document.getElementById("lhour");
+lhour.onclick = function() {
+    const now = new Date();
+    const fhour = document.getElementById("fhour");
+    const thour = document.getElementById("thour");
+
+    fhour.value = now.getHours() - 1;
+    thour.value = now.getHours();
+}
+// END MODAL SECTION
+
+// TAB SECTION
+let count = 0;
+function createTab(tagName) {
+    const all = document.getElementById(tagName);
+    const ts = document.getElementById("tab-section");
+    all.onclick = function() {
+        const comp = document.createElement("div");
+        comp.setAttribute('id','tab_'+count);
+        comp.classList.add("tab");
+        comp.classList.add("col-md-2");
+        
+        const name = tagName.split('');
+        name[0] = name[0].toUpperCase();
+        const text = document.createTextNode(name.join(''));
+        comp.appendChild(text);
+
+        const cancel = document.createElement("span");
+        cancel.classList.add("close-tab");
+        cancel.appendChild(document.createTextNode("x"));
+
+        // Close tab
+        cancel.onclick = function() {
+            const close = document.getElementById('tab_'+count);
+            close.outerHTML = '';
+        }
+
+        comp.appendChild(cancel);
+
+        ts.appendChild(comp);
+    }
+}
+createTab("all");
+createTab("add");
+createTab("delete");
+createTab("edit");
+createTab("export");
+createTab("select");
+// END TAB SECTION
